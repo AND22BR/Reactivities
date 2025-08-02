03af94995f6782edc75468818949c4cf47350daf
Andreas Dreifuerst <adreifuerst22@gmail.com>
2025-08-02 01:57:51 -0500

    Forms

--- Domain/Activity.cs
+++ Domain/Activity.cs
@@ -7,7 +7,10 @@ namespace Domain
         public DateTime Date { get; set; }
         public string Description { get; set; }
         public string Category { get; set; }
+        public bool IsCancelled { get; set; }
         public string City { get; set; }
         public string Venue { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
     }
 }
--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -10,17 +10,21 @@ import { RouterProvider } from 'react-router'
 import { router } from './app/router/Routes.tsx'
 import { store, StoreContext } from './lib/stores/store.ts'
 import { ToastContainer } from 'react-toastify'
+import { LocalizationProvider } from '@mui/x-date-pickers'
+import {AdapterDateFns} from '@mui/x-date-pickers/AdapterDateFns'
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <StoreContext.Provider value={store}>
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <StoreContext.Provider value={store}>
       <QueryClientProvider client={queryClient}>
         <ReactQueryDevtools initialIsOpen={false} />
         <ToastContainer position='top-center' hideProgressBar theme='colored'/>
         <RouterProvider router={router}/>
       </QueryClientProvider>
     </StoreContext.Provider>
+    </LocalizationProvider>
   </StrictMode>
 )
